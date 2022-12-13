c1ef144edfb4ea71fe9b2abd94d2f5f7678c0c95
Vitor Varella <120287444+VitorVarella@users.noreply.github.com>
2022-12-13 19:22:21 -0300

    Update estatisticas.py

--- estatisticas.py
+++ estatisticas.py
@@ -1,8 +1,18 @@
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# DataFrame do Banco de dados
+Dados = pd.read_excel('dados.xlsx')
 
-df = pd.DataFrame({"dados.xlsx"})
+# DataFrame da qntd de pessoas/cidade
+DPC = Dados.groupby('Cidade ').size()
+
+# DataFrame da qntd de pessoas/graduação
+DPG = DVR.groupby('Graduação').size()
+
+# DataFrame da qntd de faixa pretas
+DFPVR = DVR[DVR['Graduação']=='Preta']
+
+# mostra o box-plot dos faixa pretas de volta redonda
+DFPVR[['Idade', 'Bairro']].plot.box()
