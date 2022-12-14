91c3601b78ab8effaaa2c96214c010f06b6d9485
samuellevistrauss <68138431+samuellevistrauss@users.noreply.github.com>
2022-12-13 23:45:34 -0300

    Add files via upload

---
+++ atleta.py
@@ -0,0 +1,11 @@
+class Atleta:
+    def __init__(self,name,idade,cidade,cpf,faixa,anos,sex,time,timeaf):
+        self.name = name
+        self.idade = idade
+        self.cidade=cidade
+        self.cpf=cpf
+        self.faixa=faixa
+        self.anos=anos
+        self.sex=sex
+        self.time=time
+        self.timeaf=timeaf
