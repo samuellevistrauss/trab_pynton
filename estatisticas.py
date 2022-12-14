fecf6c2683c24167be9cde62e29cc405fef1bdd0
Vitor Varella <120287444+VitorVarella@users.noreply.github.com>
2022-12-13 23:57:49 -0300

    Update estatisticas.py

--- estatisticas.py
+++ estatisticas.py
@@ -5,15 +5,6 @@ import pandas as pd
 # DataFrame do Banco de dados
 Dados = pd.read_excel('dados.xlsx')
 
-# DataFrame do Banco de dados tratado
-Dados_alterado = 
-
-# DataFrame tratado da qntd de pessoas/bairro
-DVRbairros = Dados.groupby('Bairros').size()
-
-# DataFrame da qntd de pessoas/cidade
-DPC = Dados.groupby('Cidade ').size()
-
 # DataFrame da qntd de pessoas/graduação
 DPG = DVR.groupby('Graduação').size()
 
